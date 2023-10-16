533115c918b7a4054a0df16a83fd444644957699
ChisholmC <144708228+ChisholmC@users.noreply.github.com>
2023-10-16 09:53:02 -0300

    Guessing_Game.py

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -6,28 +6,31 @@
 #Date October 4th 2023
 #VERSION 1.82
 
-print("Hello whats your name?")
-    
-username = input("Enter username:")
-print("Hello " + username, "This is my guessing game. i will pick a number between 1 and 10 and you have 5 trys to guess it!")
-
-
-
-
 import random
 
-num = random.randint(1,10)
-guess = None
+print("Hello, what's your name?")
+username = input("Enter your username: ")
+print("Hello " + username + ", welcome to my guessing game. I will pick a number between 1 and 10, and you have 5 tries to guess it!")
 
+num = random.randint(1, 10)
+attempts = 0
+max_attempts = 5
 
-while guess !=num:
+while attempts < max_attempts:
     guess = input("Guess a number between 1 and 10: ")
+    
+    if not guess.isdigit():
+        print("Please enter a valid number.")
+        continue
+    
     guess = int(guess)
-
+    attempts += 1
 
     if guess == num:
-        print("congratulations! you won")
+        print("Congratulations, you won! The number was", num)
         break
     else:
-        print("Nope, sorry. you loose!")
-
+        if attempts < max_attempts:
+            print("Nope, sorry. You have", max_attempts - attempts, "tries left.")
+        else:
+            print("Nope, sorry. You're out of tries. The number was", num)
